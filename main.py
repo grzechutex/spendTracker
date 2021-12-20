a9aa66a921df94200a26f2b467540857a472fba9
grzec <dominik.grzeskowiak20@gmail.com>
2021-12-20 15:16:31 +0100

    ogólny zarys

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import time
+open('Categories.txt')
+
+def adding():
+    Category = input('What category? ')
+    if Category == 'new':
+        #tworzymy nową kategorie
+        CategoryMaker()
+
+
+#funkcja tworzaca nowa kategorie
+def CategoryMaker():
+    CatName = input('Name the category: ')
+    #CatColour = input('What colour should this category have?') nie wiem jak to zrobić na razie
